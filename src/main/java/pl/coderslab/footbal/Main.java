2d2ee7b07d00d1fa01dc3e317c012fa00521c6c5
arcyro <arek@arcyro.pl>
2025-08-28 14:58:42 +0200

    restTemplate

--- src/main/java/pl/coderslab/footbal/Main.java
+++ src/main/java/pl/coderslab/footbal/Main.java
@@ -9,7 +9,8 @@ public class Main {
     private static final String API_KEY
             = "eee5028bd4f1a9645f0de3b18aa4c17c11a0eedd815aeaacf2cae4d5801e8969";
     private static final String API_URL
-            = "https://apifootball.com/api/?action=get_countries&APIkey={apiKey}";
+            = "https://apiv3.apifootball.com/api/?action=get_countries&APIkey={apiKey}";
+//            = "https://apifootball.com/api/?action=get_countries&APIkey=eee5028bd4f1a9645f0de3b18aa4c17c11a0eedd815aeaacf2cae4d5801e8969";
 
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
